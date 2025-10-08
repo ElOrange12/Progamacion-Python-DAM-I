3b06713f98b6cd4454eb493cf2f1b0bab5b9fc68
Dani <danieloliveiravidal12@gmail.com>
2025-10-08 09:42:46 +0200

    Update 052-Repaso propiedades.py

--- Practicas/052-Repaso propiedades.py
+++ Practicas/052-Repaso propiedades.py
@@ -4,18 +4,4 @@ class Cliente():
     def __init__(self):
         self.nombre = ''
         self.edad = 0
-        self.telefonos = []
 
-# Ahora instancio un nuevo objeto
-cliente1 = Cliente()
-
-# Ahora le escribo una propiedad
-
-cliente1.nombre = 'Jose Vicente'
-
-print('El nombre del cliente es', cliente1.nombre)
-
-cliente1.telefonos.append('63354333')
-cliente1.telefonos.append('65436456')
-
-print(cliente1.telefonos)
