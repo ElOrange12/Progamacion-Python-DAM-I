2451f515e8b894972fec29f9aec247e23dd8244c
Dani <danieloliveiravidal12@gmail.com>
2025-10-08 10:52:06 +0200

    Create 084-Gatos y perros.py

---
+++ Practicas/084-Gatos y perros.py
@@ -0,0 +1,18 @@
+class Gato(Animal):
+	def __init__(self):
+		self.edad = 0
+		self.nombre = ''
+		self.raza = ''
+		
+class Perro(Animal):
+	def __init__(self):
+		self.edad = 0
+		self.nombre = ''
+		self.raza = ''
+		
+gato1 = Gato()
+print(gato1.edad)
+
+perro1 = Perro()
+print(perro1.edad)
+	
