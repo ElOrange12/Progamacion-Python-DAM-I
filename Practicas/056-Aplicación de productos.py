cf7f31ca58c9ce558de375ee22e228bed769d2d0
Dani <danieloliveiravidal12@gmail.com>
2025-10-08 10:25:04 +0200

    Create 056-Aplicación de productos.py

---
+++ Practicas/056-Aplicación de productos.py
@@ -0,0 +1,18 @@
+'''
+    Aplicación de gestión de productos
+    (c) 2025 Jose Vicente Carratalá
+    Esta aplicación gestiona productor
+'''
+
+# En esta aplicación no aplica librerías
+
+# Definimos clases y funciones
+
+class Producto():
+    def __init__ (self):
+        self.nombre = ''
+        self.precio = 0
+        
+# Creamos las variables globales
+
+productor = []
