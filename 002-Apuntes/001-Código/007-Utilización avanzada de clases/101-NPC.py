2ef6ed779a333a6882c7e2c06956f196db7b03f8
Dani <danieloliveiravidal12@gmail.com>
2025-11-28 08:55:56 +0100

    28/11/2025

---
+++ 002-Apuntes/001-Código/007-Utilización avanzada de clases/101-NPC.py
@@ -0,0 +1,12 @@
+# Non Playable Character
+
+class Npc():
+	def __init__ (self, x, y):
+		self.posx = x
+		self.posy = y
+		
+persona1 = Npc(4, 3)
+persona2 = Npc(5, 4)
+
+print(persona1)
+print(persona2)
