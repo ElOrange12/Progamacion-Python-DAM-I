1eb4fba8e10cc61e06ade26354b39954b6ccd9dc
Dani <danieloliveiravidal12@gmail.com>
2025-12-11 10:41:35 +0100

    11/12/2025

---
+++ 002-Apuntes/001-Código/010-Programación en el lado del servidor/900-Implatación CRUD/admin/escritorio.php
@@ -0,0 +1,36 @@
+<!-- http://localhost/GitHub/Base-de-Datos-DAM-I/002-Apuntes/008-Proyectos/003-Panel%20de%20control/aplicaci%C3%B3n/admin/escritorio.php -->
+
+<!doctype html>
+<html lang="es">
+	<head>
+		<title>El jocarsa - Panel de control</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/estilo.css">
+	</head>
+	<body>
+		<nav>
+			<button>Noticias</button>
+			<button>Autores</button>
+		</nav>
+		<main>
+			<table>
+				<thead>
+					<tr>
+						<th>Titulo</th><th>Texto</th><th>Fecha</th><th>Autor</th><th>Categoria</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						for($i = 0;$i<20;$i++){
+							echo '
+							<tr>
+							  <td>Titulo</td><td>Texto</td><td>Fecha</td><td>Autor</td><td>Categoria</td>
+							</tr>
+						  	';
+						}
+					?>
+				</tbody>
+			</table>
+		</main>
+	</body>
+</html>
